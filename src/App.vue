a963054fad0bc3c875714e191b24ecb321de85ae
yuyou <13520626847@163.com>
2020-10-27 11:23:05 +0800

    feat: 修改

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">主页</router-link> |
+    <router-link to="/about">副页</router-link>
   </div>
   <router-view />
 </template>
